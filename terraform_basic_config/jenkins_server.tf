9ad3b0463dbe50f1246a3bbec968ccaace862831
Oscar Corzo <ocorzog@psl.com.co>
2020-03-31 18:34:05 -0500

    Basic jenkins installation via script
    First approach to configure Jenkis via a script without Ansible. Next step is include Ansible in the baking process.

---
+++ terraform_basic_config/jenkins_server.tf
@@ -0,0 +1,93 @@
+# AMI lookup for this Jenkins Server
+data "aws_ami" "jenkins_server" {
+  most_recent      = true
+  owners           = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["amazon-linux-for-jenkins*"]
+  }
+}
+
+resource "aws_iam_instance_profile" "test_profile" {
+  name = "jenkins_dev_profile"
+  role = "${aws_iam_role.role.name}"
+}
+
+resource "aws_iam_role" "role" {
+  name = "jenkins_role"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+resource "aws_key_pair" "jenkins_server" {
+  key_name   = "jenkins_server"
+  public_key = "${file("jenkins_server.pub")}"
+}
+
+# lookup the security group of the Jenkins Server
+#data "aws_security_group" "jenkins_server" {
+#  filter {
+#    name   = "group-name"
+#    values = ["jenkins_server"]
+#  }
+#}
+
+# userdata for the Jenkins server ...
+data "template_file" "jenkins_server" {
+  template = "${file("../scripts/jenkins_server.sh")}"
+
+  vars {
+    env = "dev"
+    jenkins_admin_password = "mysupersecretpassword"
+  }
+}
+
+# the Jenkins server itself
+resource "aws_instance" "jenkins_server" {
+  ami                    		= "${data.aws_ami.jenkins_server.image_id}"
+  instance_type          		= "t3.medium"
+  key_name               		= "${aws_key_pair.jenkins_server.key_name}"
+  subnet_id              		= "${data.aws_subnet_ids.default_public.ids[0]}"
+  vpc_security_group_ids 		= ["${aws_security_group.jenkins_server.id}"]
+  iam_instance_profile   		= "jenkins_dev_profile"
+  user_data              		= "${data.template_file.jenkins_server.rendered}"
+
+  tags {
+    "Name" = "jenkins_server"
+  }
+
+  root_block_device {
+    delete_on_termination = true
+  }
+}
+
+output "jenkins_server_ami_name" {
+    value = "${data.aws_ami.jenkins_server.name}"
+}
+
+output "jenkins_server_ami_id" {
+    value = "${data.aws_ami.jenkins_server.id}"
+}
+
+output "jenkins_server_public_ip" {
+  value = "${aws_instance.jenkins_server.public_ip}"
+}
+
+output "jenkins_server_private_ip" {
+  value = "${aws_instance.jenkins_server.private_ip}"
+}
---
+++ terraform_basic_config/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+    profile = "personal"
+    shared_credentials_file = "~/.aws/credentials"
+    region = "us-east-1"
+}
